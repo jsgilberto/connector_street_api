e13e4dd489c8c89a232f0e32039c0674d2530d09
Jesus Alvarez <alv.mtz94@gmail.com>
2021-06-22 18:37:24 +0200

    project init

---
+++ connector_street_api/users/tests/test_models.py
@@ -0,0 +1,9 @@
+import pytest
+
+from connector_street_api.users.models import User
+
+pytestmark = pytest.mark.django_db
+
+
+def test_user_get_absolute_url(user: User):
+    assert user.get_absolute_url() == f"/users/{user.username}/"
